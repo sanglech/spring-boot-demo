15d732c681faa011b2da4156d3ca5caa37a61349
Christian <christian@sanglech.com>
2021-04-27 21:43:16 +0900

    Added some custom properties

--- src/main/java/com/christian/demo/rest/funRestController.java
+++ src/main/java/com/christian/demo/rest/funRestController.java
@@ -2,13 +2,23 @@ package com.christian.demo.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class funRestController {
-    // expose "/" that reutrns Hello world
 
+    //inject properties
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+
+    // expose "/" that reutrns Hello world
     @GetMapping("/")
     public String sayHello(){
         return "Hello World! Time on server is "+ LocalDateTime.now();
@@ -19,4 +29,7 @@ public class funRestController {
 
     @GetMapping("/fortune")
     public String fortune(){return "MIC CHECK ONE TWO ONE TWO";}
+
+    @GetMapping("/teaminfo")
+    public String teamInfo(){return "Coach: "+ coachName+ " "+ "TeamName: "+teamName;}
 }
